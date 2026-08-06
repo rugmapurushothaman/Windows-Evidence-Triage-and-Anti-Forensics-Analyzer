ddc8601670a90e3015253840c0da37c83ce0dda9
Rugma Purushothaman <rugmapurushothaman00@gmail.com>
2026-08-06 13:22:40 +0530

    Create constants.py

---
+++ utils/constants.py
@@ -0,0 +1,13 @@
+"""
+constants.py
+
+Project-wide constants.
+
+Status:
+-------
+Will be expanded as the project grows.
+"""
+
+APP_NAME = "Windows Evidence Triage & Anti-Forensics Analyzer"
+
+VERSION = "0.1.0"
