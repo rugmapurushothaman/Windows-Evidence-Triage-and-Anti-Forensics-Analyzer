2201e5b45dcddc34738f000fa100dda8f37970ec
Rugma Purushothaman <rugmapurushothaman00@gmail.com>
2026-08-06 13:20:36 +0530

    Create timeline.py

---
+++ correlation/timeline.py
@@ -0,0 +1,54 @@
+"""
+timeline.py
+
+Investigation Timeline Generator
+
+Purpose:
+--------
+Correlate forensic artifacts collected from multiple evidence sources
+to build a chronological investigation timeline.
+
+Future Responsibilities:
+- Merge timestamps from multiple collectors
+- Sort events chronologically
+- Correlate user activity
+- Correlate USB activity
+- Correlate Event Logs
+- Correlate Browser Activity
+- Correlate File Activity
+- Generate investigation timeline
+
+Input:
+------
+Artifacts collected by all collectors.
+
+Output:
+-------
+Chronological timeline of significant forensic events.
+
+Example Timeline:
+-----------------
+14:10 USB Device Connected
+
+↓
+
+14:12 powershell.exe Executed
+
+↓
+
+14:15 Security Log Cleared
+
+↓
+
+14:16 Browser History Deleted
+
+↓
+
+14:17 Recycle Bin Emptied
+
+Status:
+-------
+Planned (Version 3.0)
+"""
+
+# Implementation will be added in Version 3.0
