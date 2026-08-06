99ab674175462a695d33f15a95826c0215ca4f8d
Rugma Purushothaman <rugmapurushothaman00@gmail.com>
2026-08-06 16:36:36 +0530

    Update filesystem.py

--- collectors/filesystem.py
+++ collectors/filesystem.py
@@ -6,11 +6,6 @@ Windows Evidence Triage & Anti-Forensics Analyzer
 File System Collector
 
 Author: Rugma Purushothaman
-
-Purpose:
---------
-Scan a Windows directory and collect forensic metadata
-for every accessible file.
 """
 
 import os
@@ -35,33 +30,21 @@ def collect_files(path):
                 stats = os.stat(full_path)
 
                 file_info = {
-
-                    # File Information
                     "name": file,
                     "path": full_path,
                     "extension": os.path.splitext(file)[1],
-
-                    # Size
                     "size_bytes": stats.st_size,
-
-                    # Time Information
                     "created_time": datetime.fromtimestamp(
                         stats.st_ctime
                     ).strftime("%Y-%m-%d %H:%M:%S"),
-
                     "modified_time": datetime.fromtimestamp(
                         stats.st_mtime
                     ).strftime("%Y-%m-%d %H:%M:%S"),
-
                     "accessed_time": datetime.fromtimestamp(
                         stats.st_atime
                     ).strftime("%Y-%m-%d %H:%M:%S"),
-
-                    # File Type
                     "is_hidden": file.startswith("."),
-
                     "directory": root
-
                 }
 
                 file_list.append(file_info)
@@ -71,15 +54,10 @@ def collect_files(path):
                 FileNotFoundError,
                 OSError
             ):
-
                 continue
 
     return {
-
         "files": file_list,
-
         "file_count": len(file_list),
-
         "folder_count": folder_count
-
     }
