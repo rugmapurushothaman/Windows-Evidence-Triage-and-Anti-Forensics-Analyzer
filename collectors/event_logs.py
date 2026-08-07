fc7b8755d814e629d4d7970f0e9bd518fac80b2d
Rugma Purushothaman <rugmapurushothaman00@gmail.com>
2026-08-07 13:23:07 +0530

    Update event_logs.py

--- collectors/event_logs.py
+++ collectors/event_logs.py
@@ -1,1 +1,41 @@
-# Event Log Collector
+"""
+event_logs.py
+
+Windows Evidence Triage & Anti-Forensics Analyzer
+
+Event Log Collector
+
+Author: Rugma Purushothaman
+"""
+
+import subprocess
+
+
+def collect_security_events():
+
+    events = []
+
+    command = [
+        "wevtutil",
+        "qe",
+        "Security",
+        "/c:50",
+        "/rd:true",
+        "/f:text"
+    ]
+
+    try:
+
+        output = subprocess.check_output(
+            command,
+            text=True,
+            errors="ignore"
+        )
+
+        events.append(output)
+
+    except Exception as error:
+
+        events.append(f"Error reading Security log: {error}")
+
+    return events
